f6080727fb374f600c9fb72761922f2ec8b591d5
George <71167099+letow@users.noreply.github.com>
2023-10-11 00:45:21 +0300

    added carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { getPosts } from "./store/postsSlice";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import PostItem from "./components/PostItem";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useEffect } from "react";
 
 function App() {
-    return <div className="App"></div>;
+    const posts = useAppSelector((state) => state.toolkit.posts);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
+    return (
+        <div className="App">
+            <Carousel autoPlay={false} swipeable>
+                {posts.map((post) => (
+                    <PostItem {...post} />
+                ))}
+            </Carousel>
+        </div>
+    );
 }
 
 export default App;
