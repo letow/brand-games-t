5b835c7ea8e0b2d352bc3a0104f49177c28fe61b
George <71167099+letow@users.noreply.github.com>
2023-10-11 22:16:52 +0300

    added conditional render on status state

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,17 @@ import MainPage from "./pages/MainPage/MainPage";
 import PostPage from "./pages/PostPage/PostPage";
 import NavMenu from "./components/NavMenu/NavMenu";
 
+const enum Status {
+    LOADING = "loading",
+    RESOLVED = "resolved",
+    REJECTED = "rejected",
+}
+
 function App() {
     const dispatch = useAppDispatch();
     const posts = useAppSelector((state) => state.toolkit.posts);
+    const status = useAppSelector((state) => state.toolkit.status);
+    const error = useAppSelector((state) => state.toolkit.error);
     const [menuActive, setMenuActive] = useState(false);
 
     useEffect(() => {
@@ -27,18 +35,28 @@ function App() {
                     <span />
                 </div>
             </header>
-            <div className="app__wrapper" onClick={() => setMenuActive(false)}>
-                <Routes>
-                    {posts.map((post) => (
-                        <Route
-                            path={`/posts/${post.id}`}
-                            element={<PostPage post={post} />}
-                        />
-                    ))}
-                    <Route path={"/"} element={<MainPage />} />
-                    <Route path={"*"} element={<Navigate to={"/"} replace />} />
-                </Routes>
-            </div>
+            {status === Status.RESOLVED && (
+                <div className="app__wrapper" onClick={() => setMenuActive(false)}>
+                    <Routes>
+                        {posts.map((post) => (
+                            <Route
+                                path={`/posts/${post.id}`}
+                                element={<PostPage post={post} />}
+                            />
+                        ))}
+                        <Route path={"/"} element={<MainPage />} />
+                        <Route path={"*"} element={<Navigate to={"/"} replace />} />
+                    </Routes>
+                </div>
+            )}
+            {status === Status.LOADING && <div className="loader">Loading...</div>}
+            {status === Status.REJECTED && (
+                <div className="loader">
+                    An error occured!
+                    <br />
+                    {error}
+                </div>
+            )}
             <NavMenu isActive={menuActive} setActive={setMenuActive} posts={posts} />
         </div>
     );
